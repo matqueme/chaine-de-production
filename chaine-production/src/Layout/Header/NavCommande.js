8ff0e496f27b23ec9d1e779d88f4b3e20f85c948
matqueme <mathis.quemener@gmail.com>
2023-01-24 21:00:52 +0100

    Css barre de navigation commandes & search bar css

--- chaine-production/src/Layout/Header/NavCommande.js
+++ chaine-production/src/Layout/Header/NavCommande.js
@@ -4,31 +4,55 @@ function NavCommande(props) {
   // Verifier si cela correspond bien au bon avec l'ajout de Compléter dans la BDD
 
   return (
-    <ul class="navCommande">
-      <span class="line"></span>
-      <li class="select">
-        <Link to="/commandes">Toutes les commandes</Link>
-        <span class="lineselect"></span>
+    <ul className="navCommande">
+      <span className="line"></span>
+      <li className={props.chose === "commandes" ? "select" : "notSelect"}>
+        <Link to="/commandes">
+          Toutes les commandes
+          <span
+            className={props.chose === "commandes" ? "lineselect" : "line"}
+          ></span>
+        </Link>
       </li>
-      <li class="notSelect">
-        <Link to="/atraiter">A traiter ({props.nb_type[0].atraiter})</Link>
-        <span class="line"></span>
+      <li className={props.chose === "atraiter" ? "select" : "notSelect"}>
+        <Link to="/atraiter">
+          A traiter ({props.nb_type[0].atraiter})
+          <span
+            className={props.chose === "atraiter" ? "lineselect" : "line"}
+          ></span>
+        </Link>
       </li>
-      <li class="notSelect">
-        <Link to="/continuer">Continuer ({props.nb_type[0].continuer})</Link>
-        <span class="line"></span>
+      <li className={props.chose === "continuer" ? "select" : "notSelect"}>
+        <Link to="/continuer">
+          Continuer ({props.nb_type[0].continuer})
+          <span
+            className={props.chose === "continuer" ? "lineselect" : "line"}
+          ></span>
+        </Link>
       </li>
-      <li class="notSelect">
-        <Link to="/completer">Compléter</Link>
-        <span class="line"></span>
+      <li className={props.chose === "completer" ? "select" : "notSelect"}>
+        <Link to="/completer">
+          Compléter
+          <span
+            className={props.chose === "completer" ? "lineselect" : "line"}
+          ></span>
+        </Link>
       </li>
-      <li class="notSelect">
-        <Link to="/annulee">Annulée ({props.nb_type[0].annulee})</Link>
-        <span class="line"></span>
+      <li className={props.chose === "annulee" ? "select" : "notSelect"}>
+        <Link to="/annulee">
+          Annulée ({props.nb_type[0].annulee}){" "}
+          <span
+            className={props.chose === "annulee" ? "lineselect" : "line"}
+          ></span>
+        </Link>
       </li>
-      <li class="notSelect">
-        <Link to="/robot">Envoie au robot ({props.nb_type[0].envoirobot})</Link>
-        <span class="line"></span>
+      <li className={props.chose === "robot" ? "select" : "notSelect"}>
+        <Link to="/robot">
+          Envoie au robot ({props.nb_type[0].envoirobot}){" "}
+          <span
+            className={props.chose === "robot" ? "lineselect" : "line"}
+          ></span>
+        </Link>
       </li>
     </ul>
   );
--- chaine-production/src/Pages/Home.js
+++ chaine-production/src/Pages/Home.js
@@ -6,8 +6,6 @@ import { useLocation } from "react-router-dom";
 
 function Home() {
   const location = useLocation();
-  // The current location.
-  console.log(location.pathname.slice(1));
   // eslint-disable-next-line no-unused-vars
   const [nb_type, setNb_type] = useState([
     { atraiter: "2", continuer: "2", annulee: "0", envoirobot: "3" },
@@ -175,8 +173,26 @@ function Home() {
         permet de gérer des commandes de produits ou de services en quelques
         clics. Il permet aussi de gérer l’envoie des commandes au robot.
       </h2>
-      <NavCommande nb_type={nb_type} />
-      <button onClick={append}>append</button>
+      <NavCommande nb_type={nb_type} chose={location.pathname.slice(1)} />
+      <div className="search">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.6002 12.0498C9.49758 12.8568 8.13777 13.3333 6.66667 13.3333C2.98477 13.3333 0 10.3486 0 6.66667C0 2.98477 2.98477 0 6.66667 0C10.3486 0 13.3333 2.98477 13.3333 6.66667C13.3333 8.15637 12.8447 9.53194 12.019 10.6419C12.0265 10.6489 12.0338 10.656 12.0411 10.6633L15.2935 13.9157C15.6841 14.3063 15.6841 14.9394 15.2935 15.33C14.903 15.7205 14.2699 15.7205 13.8793 15.33L10.6269 12.0775C10.6178 12.0684 10.6089 12.0592 10.6002 12.0498ZM11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z"
+          ></path>
+        </svg>
+        <input
+          type="text"
+          className="inputsearch"
+          placeholder="Commande, mail d’utilisateur, date ..."
+        />
+      </div>
       <table>
         <thead>
           <tr>
@@ -219,7 +235,7 @@ function Home() {
           <button onClick={pageAdd}>›</button>
         </div>
       </div>
-      <p id="test"></p>
+      <button onClick={append}>append</button>
     </div>
   );
 }
