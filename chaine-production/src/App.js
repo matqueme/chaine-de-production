b773481a1f7254b9df0e747c3792425314e4f36f
matqueme <mathis.quemener@gmail.com>
2023-02-15 14:00:31 +0100

    Version 0.0.1 Reprise de zéro du projet

--- chaine-production/src/App.js
+++ chaine-production/src/App.js
@@ -1,29 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./Pages/Home";
-import Commande from "./Pages/Commande";
 import Error from "./Pages/Error";
-import Nav from "./Layout/Header/Nav";
 import Test from "./Pages/Test";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
       <Routes>
         {/* {<Route path="/" element={<Layout />}>}  */}
         {/* <Route index element={<Home />} />
         <Route path="blogs" element={<Blogs />} />
         <Route path="contact" element={<Contact />} /> */}
-        <Route path="/commandes" element={<Home />} />
-        <Route path="/atraiter" element={<Home />} />
-        <Route path="/continuer" element={<Home />} />
-        <Route path="/completer" element={<Home />} />
-        <Route path="/annulee" element={<Home />} />
-        <Route path="/robot" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Home />} />
+        <Route path="/signup" element={<Home />} />
+        <Route path="/product/:id" element={<Home />} />
+        <Route path="/order" element={<Home />} />
+        <Route path="/account" element={<Home />} />
 
         <Route path="test" element={<Test />} />
-        <Route path="commande/:id" element={<Commande />} />
         <Route path="*" element={<Error />} />
         {/* {</Route>} */}
       </Routes>
--- chaine-production/src/Pages/Commande.js
+++
@@ -1,5 +0,0 @@
-function Commande() {
-  return <p>test</p>;
-}
-
-export default Commande;
