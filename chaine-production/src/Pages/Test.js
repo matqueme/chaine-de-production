1b27a50f5f9fc664a2b9d1faa5249c534a0806cc
matqueme <mathis.quemener@gmail.com>
2023-03-08 14:32:30 +0100

    Update

--- chaine-production/src/Pages/Test.js
+++ chaine-production/src/Pages/Test.js
@@ -1,24 +1,61 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-const Test = () => {
-  const checkpage = (params) => {
-    console.log(params);
-  };
+function Test() {
+  const [isActive, setIsActive] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    console.log("test");
+    let timer = null;
+    const resetTimer = () => {
+      setIsActive(true);
+      setShowModal(false);
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        setIsActive(false);
+        setShowModal(true);
+      }, 15000); // délai de 15 secondes avant d'afficher le modal
+    };
+
+    resetTimer();
+
+    window.addEventListener("mousemove", resetTimer);
+    window.addEventListener("touchmove", resetTimer);
+    window.addEventListener("scroll", resetTimer);
+
+    return () => {
+      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener("touchmove", resetTimer);
+      window.removeEventListener("scroll", resetTimer);
+      clearTimeout(timer);
+    };
   }, []);
 
+  useEffect(() => {
+    if (!isActive) {
+      const timer = setTimeout(() => {
+        console.log("redirect");
+      }, 10000); // délai de 5 secondes avant de rediriger l'utilisateur
+      return () => clearTimeout(timer);
+    }
+  }, [isActive]);
+
+  const handleReactivate = () => {
+    setIsActive(true);
+    setShowModal(false);
+  };
+
   return (
-    <label>
-      Nombre par page :
-      <select
-        onChange={(e) => {
-          checkpage(e.target.value);
-        }}
-      ></select>
-    </label>
+    <>
+      {isActive ? <div>Contenu de la page</div> : <div>Page désactivée</div>}
+      {showModal && (
+        <div>
+          <div>La page est inactive depuis plus de 15 secondes.</div>
+          <div>Vous allez être redirigé vers la page d'accueil dans 10s.</div>
+          <button onClick={handleReactivate}>Réactiver la page</button>
+        </div>
+      )}
+    </>
   );
-};
+}
 
 export default Test;
