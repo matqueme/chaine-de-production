c9f20e83eb99a7760e8ae84d613653f26ee21856
matqueme <mathis.quemener@gmail.com>
2023-03-09 18:50:29 +0100

    Finition de la page order review

--- chaine-production/src/Pages/OrderReview.js
+++ chaine-production/src/Pages/OrderReview.js
@@ -4,11 +4,18 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
+import Lottie from "lottie-react";
+import animationData from "../Assets/verify.json";
 
 const Account = () => {
   const navigate = useNavigate();
   const [api, setApi] = useState([]);
-  const [disconnect, setDisconnect] = useState(false);
+  //pour le timer
+  const [isActive, setIsActive] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  //pour le modal de validation
+  const [animation, setAnimation] = useState(false);
+  const [animationfade, setAnimationfade] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,32 +35,9 @@ const Account = () => {
         .catch(() => {});
     };
     fetchData();
+    setAnimation(true);
+    setAnimationfade(true);
   }, [navigate]);
-
-  //if user does noting for 2 minutes, he will be disconnected
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      //remove cookies
-      //const cookies = new Cookies();
-      //cookies.remove("api_key");
-      //cookies.remove("auth_key");
-      //navigate("/signin");
-      console.log("deco");
-    }, 1000);
-
-    //create a timer of 1 minute for open the modal
-    const timeoutModal = setTimeout(() => {
-      setDisconnect(!disconnect);
-      console.log("modal");
-    }, 30000);
-
-    //si disconnect est true, on remet le timer a 0
-    if (disconnect) {
-      clearTimeout(timeoutModal);
-    }
-
-    return () => clearTimeout(timeoutModal, timeout);
-  }, [navigate, disconnect]);
 
   //click sur la croix pour deconnecter l'utilisateur
   const disconnectClick = () => {
@@ -63,6 +47,64 @@ const Account = () => {
     cookies.remove("auth_key");
     navigate("/signin");
   };
+
+  //timer
+  useEffect(() => {
+    let timer = null;
+    const resetTimer = () => {
+      setIsActive(true);
+      setShowModal(false);
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        setIsActive(false);
+        setShowModal(true);
+      }, 15000); // délai de 15 secondes avant d'afficher le modal
+    };
+
+    resetTimer();
+
+    window.addEventListener("mousemove", resetTimer);
+    window.addEventListener("touchmove", resetTimer);
+    window.addEventListener("scroll", resetTimer);
+
+    return () => {
+      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener("touchmove", resetTimer);
+      window.removeEventListener("scroll", resetTimer);
+      clearTimeout(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isActive) {
+      const timer = setTimeout(() => {
+        //remove cookies
+        const cookies = new Cookies();
+        cookies.remove("api_key");
+        cookies.remove("auth_key");
+        navigate("/signin");
+      }, 10000); // délai de 10 secondes avant de rediriger l'utilisateur
+      return () => clearTimeout(timer);
+    }
+  }, [isActive, navigate]);
+
+  const handleReactivate = () => {
+    setIsActive(true);
+    setShowModal(false);
+  };
+
+  //animation de validation
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimationfade(false);
+      //wait 1s
+      setTimeout(() => {
+        setAnimation(false);
+      }, 495);
+    }, 1200);
+    return () => clearTimeout(timer);
+  }, [animation]);
 
   return (
     <div className="account">
@@ -90,23 +132,33 @@ const Account = () => {
           status_number={order.id_type}
         />
       ))}
+      <div
+        className={`lottie-wrapper ${animationfade ? "fade-in" : "fade-out"}`}
+      >
+        {animation && (
+          <div className="lottie">
+            <h2>Commande validé</h2>
+            <Lottie
+              animationData={animationData}
+              height={150}
+              width={150}
+              loop={false}
+              //enleve  3ms au debut de l'animation
+              initialSegment={[3, 25]}
+              speed={0.75}
+            />
+          </div>
+        )}
+      </div>
 
       {/*Modal for a pop-up of deconnexion*/}
-      {disconnect && (
-        <div className="modal">
-          <div className="modal-content">
-            <div className="modal-title">
-              <h2>Deconnexion</h2>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="48"
-                viewBox="0 96 960 960"
-                width="48"
-                onClick={() => setDisconnect(false)}
-              >
-                <path d="M180 936q-24 0-42-18t-18-42V276q0-24 18-42t42-18h291v60H180v600h291v60H180Zm486-185-43-43 102-102H375v-60h348L621 444l43-43 176 176-174 174Z" />
-              </svg>
-            </div>
+      {showModal && (
+        <div className={`lottie-wrapper`}>
+          <div className="lottie">
+            <h2>Deconnexion</h2>
+            <p> Vous aller être déconnecter dans 10 secondes</p>
+
+            <button onClick={handleReactivate}>Continuer la visite</button>
           </div>
         </div>
       )}
